59ba97951be1a8a7ff68d793289ab02c259ccff4
dhlee3994 <dhlee3994@gmail.com>
2025-05-04 18:29:05 +0900

    feat: JPA BaseEntity 및 Auditing 설정 추가

---
+++ common/src/main/kotlin/com/fastcampus/commerce/common/jpa/BaseEntity.kt
@@ -0,0 +1,30 @@
+package com.fastcampus.commerce.common.jpa
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
+import jakarta.persistence.Column
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    lateinit var createdAt: LocalDateTime
+        private set
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    lateinit var updatedAt: LocalDateTime
+        private set
+
+    var deletedAt: LocalDateTime? = null
+}
