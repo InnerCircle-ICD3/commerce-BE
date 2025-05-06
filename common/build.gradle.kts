eb38c32023ae5bb07d31bc33a95dddcabd6c535e
dhlee3994 <dhlee3994@gmail.com>
2025-05-06 13:14:56 +0900

    style: 불필요한 문자열 보간 제거 및 코드 포맷 수정

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,14 +3,12 @@ val querydslVersion: String by project
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-    implementation("io.github.openfeign.querydsl:querydsl-jpa:${querydslVersion}")
-    kapt("io.github.openfeign.querydsl:querydsl-apt:${querydslVersion}:jpa")
+    implementation("io.github.openfeign.querydsl:querydsl-jpa:$querydslVersion")
+    kapt("io.github.openfeign.querydsl:querydsl-apt:$querydslVersion:jpa")
 }
 
 sourceSets {
     main {
-        java {
-            srcDirs("build/generated/source/kapt/main")
-        }
+        java.srcDirs("build/generated/source/kapt/main")
     }
 }
