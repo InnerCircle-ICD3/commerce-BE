2848a3a4fedba8e555e34b4dde1c18d5dac2d42e
dhlee3994 <dhlee3994@gmail.com>
2025-05-08 13:20:28 +0900

    chore: Gradle 설정 초기화 및 공통 플러그인, 종속성, 버전 관리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,73 @@
+val javaVersion: String by project
+val springBootVersion: String by project
+val kotestVersion: String by project
+val springKotestVersion: String by project
+
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm")
+    kotlin("kapt")
+    kotlin("plugin.spring") apply false
+    kotlin("plugin.jpa") apply false
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management")
+    id("org.jlleitschuh.gradle.ktlint") apply false
 }
 
-group = "com.fastcampus"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = project.property("projectGroup") as String
+    version = project.property("applicationVersion") as String
 
-repositories {
-    mavenCentral()
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.kapt")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
+    java.sourceCompatibility = JavaVersion.toVersion(javaVersion)
+
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+        testImplementation("io.kotest:kotest-runner-junit5:${kotestVersion}")
+        testImplementation("io.kotest:kotest-assertions-core:${kotestVersion}")
+        testImplementation("io.kotest.extensions:kotest-extensions-spring:${springKotestVersion}")
+
+        annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+        kapt("org.springframework.boot:spring-boot-configuration-processor")
+    }
+
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.boot:spring-boot-dependencies:$springBootVersion")
+        }
+    }
+
+    tasks.named<Jar>("bootJar") {
+        enabled = false
+    }
+
+    tasks.named<Jar>("jar") {
+        enabled = true
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = javaVersion
+        }
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+        systemProperty("user.timezone", "UTC")
+    }
 }
