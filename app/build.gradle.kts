b5f2fef338e1816178fdbc1fb898e4e48548969b
dhlee3994 <dhlee3994@gmail.com>
2025-05-10 16:52:21 +0900

    chore: build.gradle 의존성 추가
    - common
    - spring-starter-web
    - spring-starter-test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,7 @@
-dependencies {}
+dependencies {
+    implementation(project(":common"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
