c0180979d23edc816f289ab853f5c2c9ebbbc62e
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2025-05-10 10:11:04 +0900

    feat: jjwt 버전 변수 관리

--- modules/auth/build.gradle.kts
+++ modules/auth/build.gradle.kts
@@ -1,3 +1,6 @@
+val jjwtVersion = "0.11.5"
+//const val mockitoVersion = "4.5.1"
+
 dependencies {
     // Spring Security Core
     implementation("org.springframework.boot:spring-boot-starter-security")
@@ -6,7 +9,7 @@ dependencies {
     implementation("org.springframework.security:spring-security-oauth2-client")
 
     // JWT support (via JJWT)
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-api:${jjwtVersion}")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:${jjwtVersion}")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:${jjwtVersion}")
 }
