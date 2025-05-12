c890f83f0e5906b0c72d34e60be07b65ae9fe2fa
GwonDooHyeon <secure3141@gmail.com>
2025-05-12 21:32:38 +0900

    chore: 인증 의존성 추가

--- modules/auth/build.gradle.kts
+++ modules/auth/build.gradle.kts
@@ -1,1 +1,14 @@
-dependencies {}
+val jjwtVersion: String by project
+
+dependencies {
+    // Spring Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // Oauth Client
+    implementation("org.springframework.security:spring-security-oauth2-client")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jjwtVersion")
+}
