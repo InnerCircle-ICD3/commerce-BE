3229a39e6f6e4d3392b66603173f91e28917e895
CHOSSEANG <jng00153@gmail.com>
2025-05-12 21:42:25 +0900

    chore : admin 모듈 설정 세팅

--- modules/admin/build.gradle.kts
+++ modules/admin/build.gradle.kts
@@ -1,1 +1,20 @@
-dependencies {}
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"  // 코드 스타일 검사
+}
+
+tasks.bootJar { enabled = true}
+tasks.jar { enabled = false }
+
+dependencies {
+    // 다른모듈 의존성 추가
+    implementation(project(":modules:product"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2")
+}
