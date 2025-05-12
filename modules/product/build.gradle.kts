b0ed2cdcefbcef49b05469100c4f8e7e09212570
CHOSSEANG <jng00153@gmail.com>
2025-05-13 02:57:22 +0900

    chore : 상품 모듈 의존성 및 설정 파일 추가

--- modules/product/build.gradle.kts
+++ modules/product/build.gradle.kts
@@ -1,1 +1,17 @@
-dependencies {}
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"  // 코드 스타일 검사
+}
+
+tasks.bootJar { enabled = true}
+tasks.jar { enabled = false }
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2")
+}
