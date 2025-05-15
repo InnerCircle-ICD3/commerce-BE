c6030eefb87dc57f5106075e5c3c62ecdd85b7dd
CHOSSEANG <jng00153@gmail.com>
2025-05-16 02:57:07 +0900

    feat : 재고 entity 추가

---
+++ modules/product/src/main/kotlin/com/fastcampus/commerce/product/domain/Inventory.kt
@@ -0,0 +1,28 @@
+package com.fastcampus.commerce.product.domain
+
+import java.time.LocalDateTime
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity
+class Inventory (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    var product: Product,
+
+    @Column(name = "created_at")
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @Column(name = "updated_at")
+    var updatedAt: LocalDateTime = LocalDateTime.now(),
+)
