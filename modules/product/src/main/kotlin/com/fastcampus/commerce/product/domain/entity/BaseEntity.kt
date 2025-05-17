9d44aa206457f5887b1948153abb9ef4e61edbba
dhlee3994 <dhlee3994@gmail.com>
2025-05-17 12:00:16 +0900

    refactor: 도메인 엔티티 구조 리팩토링 및 Soft Delete 적용
    # Conflicts:
    #	modules/product/src/main/kotlin/com/fastcampus/commerce/product/domain/Category.kt
    #	modules/product/src/main/kotlin/com/fastcampus/commerce/product/domain/CategoryGroup.kt
    #	modules/product/src/main/kotlin/com/fastcampus/commerce/product/domain/Product.kt
    #	modules/product/src/main/kotlin/com/fastcampus/commerce/product/domain/ProductCategory.kt
    #	modules/product/src/main/kotlin/com/fastcampus/commerce/product/domain/ProductCategoryEntity.kt
    #	modules/product/src/main/kotlin/com/fastcampus/commerce/product/domain/entity/ProductCategory.kt

---
+++ modules/product/src/main/kotlin/com/fastcampus/commerce/product/domain/entity/BaseEntity.kt
@@ -0,0 +1,16 @@
+package com.fastcampus.commerce.product.domain.entity
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+
+@EntityListeners(AuditingEntityListener::class)
+@MappedSuperclass
+abstract class BaseEntity {
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    lateinit var createdAt: LocalDateTime
+}
