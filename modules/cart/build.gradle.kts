d29dcaf904be5197efcfac09357a29077b49eff0
CHOSSEANG <jng00153@gmail.com>
2025-05-17 12:38:41 +0900

    chore(product) : 의존성 및 plugin 추가

--- modules/cart/build.gradle.kts
+++ modules/cart/build.gradle.kts
@@ -1,1 +1,18 @@
-dependencies {}
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("org.jlleitschuh.gradle.ktlint") apply false
+}
+
+tasks.bootJar { enabled = true}
+tasks.jar { enabled = false }
+
+dependencies {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2")
+}
