7af76eb84b1e39e598fab626eb71d6e844e03b90
dhlee3994 <dhlee3994@gmail.com>
2025-05-17 11:58:49 +0900

    chore: Querydsl 의존성 및 설정 추가

--- modules/product/build.gradle.kts
+++ modules/product/build.gradle.kts
@@ -1,16 +1,31 @@
+val springMockkVersion: String by project
+val querydslVersion: String by project
+
 plugins {
     kotlin("jvm")
+    kotlin("kapt")
     kotlin("plugin.spring")
+    kotlin("plugin.jpa")
     id("org.springframework.boot")
     id("io.spring.dependency-management")
+    id("org.jlleitschuh.gradle.ktlint")
 }
-
-tasks.bootJar { enabled = true}
-tasks.jar { enabled = false }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("org.postgresql:postgresql")
-    runtimeOnly("com.h2database:h2")
+    implementation(project(":common"))
+
+    implementation("io.github.openfeign.querydsl:querydsl-core:$querydslVersion")
+    implementation("io.github.openfeign.querydsl:querydsl-jpa:$querydslVersion")
+    kapt("io.github.openfeign.querydsl:querydsl-apt:$querydslVersion:jpa")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.ninja-squad:springmockk:$springMockkVersion")
 }
+
+kapt {
+    correctErrorTypes = true
+}
+
+val querydslDir = "build/generated"
+
+sourceSets["main"].java.srcDirs(querydslDir)
