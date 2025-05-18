336ec7969fe309b63aff95e64c4650c5b9c2cf8d
CHOSSEANG <jng00153@gmail.com>
2025-05-18 23:45:01 +0900

    feat(cart): api response 형식 변경

--- modules/cart/src/main/kotlin/com/fastcampus/commerce/cart/interfaces/CartController.kt
+++ modules/cart/src/main/kotlin/com/fastcampus/commerce/cart/interfaces/CartController.kt
@@ -2,7 +2,6 @@ package com.fastcampus.commerce.cart.interfaces
 
 import com.fastcampus.commerce.cart.application.CartService
 import com.fastcampus.commerce.common.response.ApiResponse
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,7 +15,7 @@ class CartController(
     @PostMapping
     fun addToCart(
         @RequestBody request: CartAddRequest,
-    ): ResponseEntity<ApiResponse<CartAddResponse>> {
+    ): ApiResponse<CartAddResponse> {
         /*
         임시로 userId 부여
         USER 모듈 완성 후 수정 예정
@@ -24,6 +23,6 @@ class CartController(
         val userId = 1L
 
         val response = cartService.addToCart(userId, request)
-        return ResponseEntity.ok(ApiResponse.success(response))
+        return ApiResponse.success(response)
     }
 }
